29ccabbf87051b9e04a68fe9b4f962ca5ec89f84
Pablo Cortez <cspablocortez@gmail.com>
2023-05-26 16:42:31 -0700

    Update main.py
    Added infinite control loop

--- main.py
+++ main.py
@@ -15,3 +15,15 @@ print("Listening...")
 autoclicker_enabled = False
 
 # Infinite loop
+while True:
+    if keyboard.is_pressed(",") and autoclicker_enabled == False:
+        autoclicker_enabled = True
+        print("ON")
+        while autoclicker_enabled:
+            sleep(0.01)
+            start_clicking()
+
+            if keyboard.is_pressed(",") and autoclicker_enabled:
+                autoclicker_enabled = False
+                print("OFF")
+                sleep(0.1)
